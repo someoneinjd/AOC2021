d9bbfaca196268661b20ad7504082e0d8baa2a87
someoneinjd <someoneinjd@outlook.com>
2022-02-17 10:16:05 +0800

    :sparkles: Add day13

--- xmake.lua
+++ xmake.lua
@@ -52,7 +52,8 @@ end
 local day = {
     'day01', 'day02', 'day03', 'day04',
     'day05', 'day06', 'day07', 'day08',
-    'day09', 'day10', 'day11', 'day12'
+    'day09', 'day10', 'day11', 'day12',
+    'day13'
 }
 
 for _, name in ipairs(day) do 
