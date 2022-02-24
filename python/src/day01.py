a0f5d5d26faad676dcef69f562e44b220fe76e31
someoneinjd <someoneinjd@outlook.com>
2022-02-24 14:38:03 +0800

    :recycle: Refactor benchmark

--- python/src/day01.py
+++ python/src/day01.py
@@ -15,7 +15,7 @@ data = open("input/input01.txt").read()
 res1 = solution1(data)
 res2 = solution2(data)
 print(
-    "{}\n{}\nElapsed Time 1:\n\t{}ms\nElasped Time 2:\n\t{}ms".format(
+        "{}\n{}\nElapsed Time 1:\n\t{:.6f}ms\nElasped Time 2:\n\t{:.6f}ms".format(
         res1,
         res2,
         timeit('solution1(data)', number=100, globals=globals()) * 10.0,
