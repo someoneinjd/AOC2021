2b11cb2bf552f4b9de4914084bce2baf3b172708
someoneinjd <someoneinjd@outlook.com>
2022-02-28 09:47:12 +0800

    :art: Format day01 (python)

--- python/src/day01.py
+++ python/src/day01.py
@@ -15,10 +15,10 @@ data = open("input/input01.txt").read()
 res1 = solution1(data)
 res2 = solution2(data)
 print(
-        "{}\n{}\nElapsed Time 1:\n\t{:.6f}ms\nElasped Time 2:\n\t{:.6f}ms".format(
+    "{}\n{}\nElapsed Time 1:\n\t{:.6f}ms\nElasped Time 2:\n\t{:.6f}ms".format(
         res1,
         res2,
-        timeit('solution1(data)', number=100, globals=globals()) * 10.0,
-        timeit('solution2(data)', number=100, globals=globals()) * 10.0
+        timeit("solution1(data)", number=100, globals=globals()) * 10.0,
+        timeit("solution2(data)", number=100, globals=globals()) * 10.0,
     )
 )
